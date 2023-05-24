e7d8b92294bff4bb3b6cdcb1352f07f644d26883
simplystudios <new.anshwadhwa@gmail.com>
2023-05-24 16:59:14 +0530

    revamped

--- advancedsearch/app.js
+++ advancedsearch/app.js
@@ -1,71 +1,135 @@
+const duckDuckGoApiUrl = "https://api.duckduckgo.com/?format=json&q=";
+const searchEngineApiUrl = "https://search-engine-api.p.rapidapi.com/api/Search/";
+
 const Container = document.getElementById("search-results");
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
-const apiUrl = "https://en.wikipedia.org/w/rest.php/v1/search/page?q=";
 const Pagetitle = document.getElementById("pgt");
 
-let performSearch = () => {
-    const query = searchInput.value;
-    if (query.trim() === "") {
-        Container.textContent = "Please enter a search term.";
-        return;
-    }
-
-    const url = `${apiUrl}${query}`;
-    Pagetitle.textContent = `${query} - Keepitshort`; // Set the page title
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                Container.textContent = data.error;
-            } else {
-                const pages = data.pages;
-                if (pages.length > 0) {
-                    Container.innerHTML = ""; // Clear previous results
-                    pages.forEach(page => {
-                        const listItem = document.createElement("div");
-                        listItem.classList.add("list-item");
-
-                        const title = document.createElement("h3");
-                        title.classList.add("center");
-                        title.textContent = page.title;
-
-                        const excerpt = document.createElement("p");
-                        excerpt.classList.add("center");
-                        excerpt.innerHTML = page.excerpt.replace(/<span[^>]*>(.*?)<\/span>/gi, "$1");
-
-                        const image = document.createElement("img");
-                        image.src = page.thumbnail ? modifyImageUrl(page.thumbnail.url, 330, 150) : "";
-                        image.alt = page.title;
-                        image.classList.add("thumbnail");
-
-                        listItem.appendChild(title);
-                        listItem.appendChild(image);
-                        listItem.appendChild(excerpt);
-                        Container.appendChild(listItem);
-                    });
-                } else {
-                    Container.textContent = "No results found.";
-                }
-            }
-        })
-        .catch(error => {
-            Container.textContent = "An error occurred during the search.";
-            console.error(error);
-        });
+// Function to clear previous search results
+const clearResults = () => {
+  while (Container.firstChild) {
+    Container.removeChild(Container.firstChild);
+  }
 };
 
 // Function to modify the image URL to the desired size
 const modifyImageUrl = (url, size) => {
-    const filename = url.substring(url.lastIndexOf('/') + 1);
-    const modifiedUrl = `${url.substring(0, url.lastIndexOf('/') + 1)}${size}px-${filename}`;
-    return modifiedUrl;
+  const filename = url.substring(url.lastIndexOf("/") + 1);
+  const modifiedUrl = `${url.substring(0, url.lastIndexOf("/") + 1)}${size}px-${filename}`;
+  return modifiedUrl;
 };
-let handleKeyPress = event => {
-    if (event.keyCode === 13 || event.which === 13) {
-        performSearch();
-    }
+
+const createListItem = (title, image, description, link) => {
+  const listItem = document.createElement("div");
+  listItem.classList.add("list-item");
+  listItem.classList.add("searchr");
+
+  const listItemContent = document.createElement("a");
+  listItemContent.href = link;
+  listItemContent.target = "_blank"; // Open link in a new tab
+  listItemContent.rel = "noopener noreferrer"; // Set recommended security attributes
+
+  const titleElement = document.createElement("h3");
+  titleElement.classList.add("center");
+  titleElement.textContent = title;
+
+  const descriptionElement = document.createElement("p");
+  descriptionElement.classList.add("center");
+  descriptionElement.innerHTML = description;
+
+  listItemContent.appendChild(titleElement);
+  listItemContent.appendChild(descriptionElement);
+  listItem.appendChild(listItemContent);
+
+  return listItem;
+};
+
+
+const performSearch = () => {
+  const query = searchInput.value;
+  if (query.trim() === "") {
+    Container.textContent = "Please enter a search term.";
+    return;
+  }
+
+  clearResults(); // Clear previous search results
+
+  const duckDuckGoUrl = `${duckDuckGoApiUrl}${query}`;
+
+  fetch(duckDuckGoUrl)
+    .then((response) => response.json())
+    .then((duckDuckGoData) => {
+      if (duckDuckGoData.Abstract) {
+        const wikiUrl = duckDuckGoData.AbstractURL;
+        const abstract = duckDuckGoData.Abstract;
+        const heading = duckDuckGoData.Heading;
+        const image = duckDuckGoData.Image;
+
+        const listItem = document.createElement("div");
+        listItem.classList.add("list-item");
+        listItem.classList.add("duckduckgo");
+
+        const listItemContent = document.createElement("a");
+        listItemContent.href = wikiUrl;
+        listItemContent.target = "_blank"; // Open link in a new tab
+        listItemContent.rel = "noopener noreferrer"; // Set recommended security attributes
+
+        const title = document.createElement("h3");
+        title.classList.add("center");
+        title.textContent = heading;
+
+        const excerpt = document.createElement("p");
+        excerpt.classList.add("center");
+        excerpt.textContent = abstract;
+
+
+        listItemContent.appendChild(title);
+        listItemContent.appendChild(excerpt);
+        listItem.appendChild(listItemContent);
+        Container.appendChild(listItem);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+
+  // Fetch search results from the additional search engine API
+  const searchurl = `${searchEngineApiUrl}${query}?numOfPages=1`;
+  fetch(searchurl, {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": sh29ah350194957382,
+      "X-RapidAPI-Host": "search-engine-api.p.rapidapi.com",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        Container.textContent = data.error;
+      } else {
+        if (data.length > 0) {
+          data.forEach((item) => {
+            const listItem = createListItem(item.title, item.image, item.description, item.link);
+            Container.appendChild(listItem);
+          });
+        } else {
+          Container.textContent = "No results found.";
+        }
+      }
+    })
+    .catch((error) => {
+      Container.textContent = "An error occurred during the search.";
+      console.error(error);
+    });
+};
+
+// ...
+const handleKeyPress = (event) => {
+  if (event.keyCode === 13 || event.which === 13) {
+    performSearch();
+  }
 };
 
 searchButton.addEventListener("click", performSearch);
